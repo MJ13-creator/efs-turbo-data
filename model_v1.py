f11178d7eb73f6f2f077bdd5a32a3f1f4a16cc68
MJ13-creator <manoj.jagadeesh@alten-india.com>
2026-09-08 15:56:08 +0530

    Update model_v1.py

--- model_v1.py
+++ model_v1.py
@@ -7,6 +7,18 @@ from streamlit_sortables import sort_items
 from werkzeug.security import generate_password_hash, check_password_hash
 from supabase import create_client, Client
 
+hide_streamlit_style = """
+<style>
+div[data-testid="stToolbar"] {visibility: hidden; height: 0%; position: fixed;}
+div[data-testid="stDecoration"] {visibility: hidden; height: 0%; position: fixed;}
+div[data-testid="stStatusWidget"] {visibility: hidden; height: 0%; position: fixed;}
+#MainMenu {visibility: hidden; height: 0%;}
+header {visibility: hidden; height: 0%;}
+footer {visibility: hidden; height: 0%;}
+</style>
+"""
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
+
 # ══════════════════════════════════════════════════════════════════════════════
 #  CONFIG / CONSTANTS
 # ══════════════════════════════════════════════════════════════════════════════
